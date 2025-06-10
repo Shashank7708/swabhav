f43c213ae0a408cda37360875f3bbe4cce05b3c7
Shashank7708 <shetyeshashank04@gmail.com>
2025-06-10 12:56:34 +0100

    Generic

---
+++ New Jorney/BinarySearch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection.Metadata;
+
+namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = [10, 17, 25, 35, 52, 55, 62, 77, 90, 95];
+            var result = BinarySearch(arr, 62);
+            Console.WriteLine("Value found at index: "+result+" using Binary Search");
+            int[] arr2 = [1, 3, 3, 5, 7];
+            var resultUpperBound = FindUpperBound(arr2, 62);
+            Console.WriteLine("Upper bound found at index: " + resultUpperBound + " using Binary Search");
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+        private static int BinarySearch(int[] arr,int key)
+        {
+            int minVal = 0;
+            int maxVal = arr.Length - 1;
+            while (minVal <= maxVal)
+            {
+                          int mid= (minVal+maxVal)/2;
+                if (key == arr[mid])
+                    return mid++;
+                else if (key < arr[mid])
+                    maxVal = mid ;
+                else
+                    minVal = mid ;
+            }
+            return -1;
+        }
+        
+        private static int FindUpperBound(int[] arr,int key)
+        {
+            int left = 0;
+            int right = arr.Length;
+            while(left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (arr[mid] <= key)
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+            return left;
+        }
+    }
+}
---
+++ New Jorney/LinkedList LeetCode2/Program.cs
@@ -0,0 +1,145 @@
+﻿using System.Numerics;
+using System.Xml.Linq;
+
+namespace LinkedList_LeetCode2
+{
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+  }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList myList1 = new LinkedList();
+
+            
+            
+            myList1.AddFirst(7);
+            myList1.AddFirst(5);myList1.AddFirst(3);
+            myList1.AddFirst(8);
+
+            myList1.AddFirst(6);
+
+            myList1.AddFirst(5);
+
+            myList1.AddFirst(6);
+
+            myList1.AddFirst(8);
+
+            myList1.AddFirst(0);
+            LinkedList myList2 = new LinkedList();
+
+            myList2.AddFirst(7);
+            myList2.AddFirst(9);
+            myList2.AddFirst(8);
+            myList2.AddFirst(5);
+            myList2.AddFirst(8);
+            myList2.AddFirst(0);
+            myList2.AddFirst(8);
+            myList2.AddFirst(7);
+            myList2.AddFirst(6);
+            var abc = AddTwoNumbers(myList1.head, myList2.head);
+        }
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var value1 = ReverseNo(GetNofromListNode(l1));
+            var value2 = ReverseNo(GetNofromListNode(l2));
+            BigInteger result = BigInteger.Parse(value1) + BigInteger.Parse(value2);
+            string r1 = result.ToString();
+            ListNode head = null;
+            for (int i = r1.Length-1; i>=0; i--)
+            {
+                int val = int.Parse(r1[i].ToString());
+                ListNode n = new ListNode(val);
+                n.next = head;
+                head = n;
+
+            }
+            return head;
+        }
+
+        static string GetNofromListNode(ListNode l1)
+        {
+            string temp = "";
+            ListNode runner = l1;
+            while (runner != null)
+            {
+                temp = temp + runner.val.ToString();
+                runner = runner.next;
+            }
+            return temp;
+        }
+        static string ReverseNo(string x)
+        {
+           // BigInteger reversenum = 0;
+            //BigInteger temp = x;
+            /*while (temp != 0)
+            {
+                BigInteger val = temp % 10;
+                reversenum = reversenum * 10 + val;
+                temp = temp / 10;
+            }
+            */
+            char[] arr = x.ToString().ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+    public class LinkedList
+    {
+        public ListNode head;
+
+        public void printAllNodes()
+        {
+            ListNode current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.val);
+                current = current.next;
+            }
+        }
+
+        public void AddFirst(int data)
+        {
+            ListNode toAdd = new ListNode();
+
+            toAdd.val = data;
+            toAdd.next = head;
+
+            head = toAdd;
+        }
+/*
+        public void AddLast(Object data)
+        {
+            if (head == null)
+            {
+                head = new Node();
+
+                head.data = data;
+                head.next = null;
+            }
+            else
+            {
+                Node toAdd = new Node();
+                toAdd.data = data;
+
+                Node current = head;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+
+                current.next = toAdd;
+            }
+        }
+*/
+    }
+}
