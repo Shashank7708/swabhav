9a14abd700ab20751a7e6d3b5750171ec091ded1
Shashank <shetyeshashank04@gmail.com>
2025-06-20 22:14:07 +0100

    Started 2 pointers problems LeetCode

---
+++ LeetCode 75/StringComparison443/StringComparison443/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringComparison443
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string arr = "a";
+            var result = Compress(arr.ToCharArray());
+            Console.Write(result);
+            Console.Read();
+
+        }
+
+        public static int Compress(char[] chars)
+        {
+            int charCount = 0; int uniqueCount = 0;
+            if (chars.Length == 1)
+                return charCount + 1;
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                char ch = chars[i];
+                charCount = 1;
+                while (chars.Length > i + 1 && chars[i + 1] == ch)
+                {
+                    charCount++;
+                    i++;
+                }
+                chars[uniqueCount++] = ch;
+                if (charCount > 1)
+                {
+                    foreach (var c in charCount.ToString())
+                        chars[uniqueCount++] = c;
+                }
+            }
+            Array.Resize(ref chars, uniqueCount);
+            return uniqueCount;
+        }
+    }
+}
