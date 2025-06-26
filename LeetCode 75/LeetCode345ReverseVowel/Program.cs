3068a0721565e8696a14d966e8e0993dcdc7c08b
Shashank <shetyeshashank04@gmail.com>
2025-06-26 17:34:26 +0100

    LeetCode Poointer

--- LeetCode 75/LeetCode345ReverseVowel/Program.cs
+++ LeetCode 75/LeetCode345ReverseVowel/Program.cs
@@ -7,12 +7,43 @@ namespace LeetCode345ReverseVowel
         static void Main(string[] args)
         {
             string s = "IceCreAm";
-            ReverseVowels(s);
+            ReverseVowelsApp1(s);
             Console.WriteLine("Hello, World!");
             Console.Read();
         }
 
-        public static string ReverseVowels(string s)
+        public static string ReverseVowelsApp1(string s)
+        {
+            char[] arr = s.ToCharArray();
+
+            int i = 0, end = s.Length - 1;
+            while (i < end)
+            {
+
+                while (i < end && !isVowel(arr[i]))
+                {
+                    i++;
+                }
+
+                while (i < end && !isVowel(arr[end]))
+                {
+                    end--;
+                }
+
+                (arr[i], arr[end]) = (arr[end], arr[i]);
+
+                i++; end--;
+            }
+            return new String(arr);
+        }
+
+        static bool isVowel(char c)
+        {
+            return c is 'a' or 'A' or 'e' or 'E' or 'i' or 'I' or 'o' or 'O' or 'u' or 'U';
+        }
+
+
+        public static string ReverseVowelsApp2(string s)
         {
             StringBuilder sb = new StringBuilder();
             Stack<char> vowelStack = new Stack<char>();
