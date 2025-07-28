56b5f73e3fdf442a30dbcbeb54821020c11a129e
Shashank7708 <shetyeshashank04@gmail.com>
2025-07-28 08:30:35 +0100

    commit 28072025

---
+++ New Jorney/Basic/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Basic
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            /*int a;
+            float b;
+            bool c;
+            char d  ;
+            string e=null;
+            Console.WriteLine("Default values of Different type of Integers:");
+            Console.WriteLine($"Integer: {a}\nFloat: {b}\nbool: {c}\nChar: {d}\nstring: {e}");
+            */
+            int[] arrint = new int[10];
+            for (int i = 0; i < arrint.Length; i++)
+            {
+                Console.WriteLine(arrint[i]);
+            }
+            
+                string[] strs = new string[] { "ab", "a" };
+                if (strs.Length == 1)
+                    Console.WriteLine(strs[0]);
+
+                foreach (string s in strs)
+                    if (String.IsNullOrEmpty(s))
+                        Console.WriteLine("");
+
+                for (int i = 0; i < strs[0].Length; i++)
+                {
+                    char ch = strs[0][i];
+                    for (int j = 1; j < strs.Length; j++)
+                    {
+                        if (i >= strs.Length || strs[j][i] != ch)
+                            Console.WriteLine(strs[0].Substring(0, i));
+                    }
+                }
+                Console.WriteLine(""); 
+    
+            Dummy r = new Dummy();
+            Console.WriteLine(r.w);
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+
+    class Dummy
+    {
+       internal int q;
+       internal int w;
+    }
+}
---
+++ New Jorney/ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using HRAdmistrationApi;
+using System.Linq;
+namespace ConsoleApp1
+{
+    public enum EmployeeType
+    {
+        Teacher,
+        HeadOfDepartment,
+        DeputyHeadMaster,
+        HeadMaster
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal totalSaalry = 0;
+            List<IEmployee> employees = new List<IEmployee>();
+            SeedData(employees);
+            //foreach(var emp in employees)
+            //{
+            //    totalSaalry = totalSaalry + emp.Salary;
+            //}
+            //Console.WriteLine($"Total Annual Salary (including Bonus)= {totalSaalry}");
+
+            //Using Linq
+            Console.WriteLine($"Total Annual Salary (including Bonus)= {employees.Sum(x => x.Salary)}");
+
+            Console.Read();
+        }
+        
+        public static void SeedData(List<IEmployee> employees)
+        {
+            IEmployee employee1= EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher,1, "Bob", "Fisher",4M);
+            employees.Add(employee1);
+
+            IEmployee teacher2 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 2, "Ronty", "Muscle", 600M); 
+            employees.Add(teacher2);
+
+            IEmployee headOfDepartment1 = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadOfDepartment, 3, "brandon", "Clark", 50M);
+            employees.Add(headOfDepartment1);
+
+            IEmployee headOfDepartment2 = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadOfDepartment, 4, "Tyson", "Varghese", 70M);
+            employees.Add(headOfDepartment2);
+        }
+    }
+
+    public class Teacher : Employee
+    {
+        public override decimal Salary { get => base.Salary + base.Salary * 0.2m; }
+    }
+    public class HeadOfDepartment : Employee
+    {
+        public override decimal Salary { get => base.Salary + base.Salary * 0.6m; }
+
+    }
+
+    public abstract class EmployeeFactory
+    {
+        public static IEmployee GetEmployeeInstance(EmployeeType employeeType,int id, string firstName,string lastName,decimal salary)
+        {
+            IEmployee employee = null;
+            switch (employeeType)
+            {
+                case EmployeeType.Teacher: 
+                    employee=new Teacher { Id = id, FirstName =firstName,LastName=lastName,Salary=salary }; 
+                    break;
+                case EmployeeType.HeadOfDepartment:
+                    employee = new HeadOfDepartment { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                default: 
+                    break;
+
+            }
+            return employee;
+        }
+    }
+
+}
