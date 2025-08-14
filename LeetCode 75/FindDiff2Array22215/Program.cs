feb8c680873c0d2a499efe6ac96340cd54536494
Shashank <shetyeshashank04@gmail.com>
2025-08-14 19:42:47 +0100

    14-08-2025

---
+++ LeetCode 75/FindDiff2Array22215/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDiff2Array22215
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 2, 3 };
+            int[] nums2 = { 2, 4, 6 };
+            FindDifference(nums1, nums2);
+            Console.Read();
+        }
+
+        public static List<List<int>> FindDifference(int[] nums1, int[] nums2)
+        {
+            Dictionary<int, int> dict1 = new Dictionary<int, int>();
+            Dictionary<int, int> dict2 = new Dictionary<int, int>();
+            foreach (var i in nums1)
+            {
+                if (!dict1.ContainsKey(i))
+                    dict1.Add(i, 1);
+            }
+            foreach (var i in nums2)
+            {
+                if (!dict2.ContainsKey(i))
+                    dict2.Add(i, 1);
+            }
+            List<List<int>> returnLst = new List<List<int>>();
+            HashSet<int> h1 = new HashSet<int>();
+            HashSet<int> h2 = new HashSet<int>();
+            foreach (int i in nums1)
+            {
+                if (!dict2.ContainsKey(i))
+                    h1.Add(i);
+            }
+            foreach (int i in nums2)
+            {
+                if (!dict1.ContainsKey(i))
+                    h2.Add(i);
+            }
+            returnLst.Add(h1.ToList());
+            returnLst.Add(h2.ToList());
+            return returnLst;
+        }
+    }
+}
---
+++ LeetCode 75/FindHighestAltitude1732/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindHighestAltitude1732
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { -5, 1, 5, 0, -7 };
+            var highestAlt = LargestAltitude(arr);
+            Console.WriteLine(highestAlt);
+            Console.Read();
+        }
+        public static int LargestAltitude(int[] gain)
+        {
+            int currentVal = 0;
+            int maxVal = 0;
+            foreach (int g in gain)
+            {
+                currentVal = currentVal + g;
+                maxVal = Math.Max(currentVal, maxVal);
+            }
+            return maxVal;
+        }
+
+        /*public static int LargestAltitude(int[] gain) {
+            int len=gain.Length;
+            int[] arr=new int[len+1];
+            arr[0]=0;
+            int maxVal=arr[0];
+            for(int i=0;i<len;i++){
+                arr[i+1]=arr[i]+gain[i];
+                maxVal=Math.Max(arr[i+1],maxVal);
+            }
+            return maxVal;
+
+        }*/
+    }
+}
